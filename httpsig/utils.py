324e1dc0be25a3f2700f534a840054828ff712e2
Reverb Chu <reverbc@me.com>
2017-07-29 15:40:22 -0700

    remove unused methods

--- httpsig/utils.py
+++ httpsig/utils.py
@@ -144,21 +144,6 @@ def build_signature_template(key_id, algorithm, headers):
     return sig_string
 
 
-def lkv(d):
-    parts = []
-    while d:
-            len = struct.unpack('>I', d[:4])[0]
-            bits = d[4:len+4]
-            parts.append(bits)
-            d = d[len+4:]
-    return parts
-
-def sig(d):
-    return lkv(d)[1]
-
-def is_rsa(keyobj):
-    return lkv(keyobj.blob)[0] == "ssh-rsa"
-
 # based on http://stackoverflow.com/a/2082169/151401
 class CaseInsensitiveDict(dict):
     def __init__(self, d=None, **kwargs):
